32e69e3a14866fd8a224a54013895b14bd42fc7e
Piyush Garg <piyushgarg809@gmail.com>
2025-03-02 15:30:04 +0530

    finished

--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -18,7 +18,7 @@ const Header = (props) => {
   }
   return (
     <div className='flex item-end justify-between text-white  '>
-        <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'> userName 👋</span> </h1>
+        <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'> Admin 👋</span> </h1>
         <button onClick={logOutUser} className="bg-red-600 hover:bg-red-700 text-lg font-medium text-white px-6 py-2 rounded-md shadow-md">Log Out</button>
 
     </div>
